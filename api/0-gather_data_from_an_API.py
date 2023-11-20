34576d19876fef4591c7b468d61836d66780571a
connortrue <connor.true.94@gmail.com>
2023-11-20 10:26:49 -0600

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""Yep"""
+""" Requests the user information from a given API """
 import requests
-import sys
-
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
 
 
-def todo_list_progress(employee_id):
-    users = requests.get(users_url).json()
-    todos = requests.get(todos_url).json()
+def display_employee_progress(employee_id):
+    """ script must display to stdout the employee todo list progress """
+    url = "https://jsonplaceholder.typicode.com"
+    empu = f"{url}/users/{employee_id}"
+    todo = f"{url}/todos"
 
-    user = next((user for user in users if user['id'] == employee_id), None)
-    if not user:
-        print("User not found")
+    try:
+        emps = requests.get(empu).json()
+        todos = requests.get(todo,
+                             params={"userId": employee_id}).json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return
 
-    user_todos = [todo for todo in todos if todo['userId'] == employee_id]
-    done_tasks = [todo for todo in user_todos if todo['completed']]
+    name = emps.get("name")
+    if name is None:
+        print(f"No employee found with ID {employee_id}")
+        return
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(user['name'], len(done_tasks), len(user_todos)))
+    complete = [t["title"] for t in todos if t["completed"]]
+    done = len(complete)
+    total = len(todos)
 
-    for task in done_tasks:
-        print("\t {}".format(task['title']))
+    print(f"Employee {name} is done with tasks({done}/{total}):")
+    for task in complete:
+        print(f"\t {task}")
 
 
 if __name__ == "__main__":
-    todo_list_progress(int(sys.argv[1]))
+    import sys
+
+    display_employee_progress(int(sys.argv[1]))
