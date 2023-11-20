7e148bb5ad0270308187e5bf1eabeb6992cf265b
connortrue <connor.true.94@gmail.com>
2023-11-20 12:23:17 -0600

    0 stolen, testing if my checker is bugged

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-"""
-    Python script that gathers data from the
-    provided REST API
-"""
+""" Requests the user information from a given API """
+import requests
+
+
+def display_employee_progress(employee_id):
+    """ script must display to stdout the employee todo list progress """
+    url = "https://jsonplaceholder.typicode.com"
+    empl_url = f"{url}/users/{employee_id}"
+    todo_url = f"{url}/todos"
+
+    empl_data = requests.get(empl_url).json()
+    todo_data = requests.get(todo_url,
+                             params={"userId": employee_id}).json()
+
+    empl_name = empl_data.get("name")
+    completed_tasks = [t["title"] for t in todo_data if t["completed"]]
+    num_done = len(completed_tasks)
+    num_total = len(todo_data)
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(empl_name, num_done, num_total))
+    for task in completed_tasks:
+        print(f"\t {task}")
+
 
 if __name__ == "__main__":
-    import requests
-    from sys import argv
-    if len(argv) < 2:
-        exit()
+    import sys
 
-    completed_tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
-        .format(argv[1]))
-    completed_tasks = completed_tasks.json()
-
-    employee_name = requests.get(
-        "https://jsonplaceholder.typicode.com/users?id={}"
-        .format(argv[1]))
-    employee_name = employee_name.json()
-    employee_name = employee_name[0]["name"].ljust(20)
-
-    remaining_tasks = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
-    remaining_tasks = len(remaining_tasks.json())
-
-    completed_list = []
-    for task in completed_tasks:
-        completed_list.append("\t {}".format(task["title"]))
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(completed_tasks), remaining_tasks))
-
-    for task in completed_list:
-        print(task)
+    display_employee_progress(int(sys.argv[1]))
